dfead09dc5396fc1b4e83a527524b77d1762eeee
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-19 11:17:56 +0600

    first commit

---
+++ Template-string.js
@@ -0,0 +1,14 @@
+const multipleLine = `This is first Line
+This is second Line
+This is third Line
+This is fourth Line
+`
+// console.log(multipleLine);
+
+const count = 5;
+const friendNum = `<p> friend Number is ${count} </p>`
+// console.log(friendNum );
+const fristName= 'shuvo';
+const lastName = 'Ghosh';
+const fulName =`${fristName} ${lastName}`
+console.log(fulName);
---
+++ default-parameter.js
@@ -0,0 +1,6 @@
+function add(num1,num2=4){
+    const number = num1 + num2;
+    return number;
+}
+const result = add(12);
+console.log(result);
